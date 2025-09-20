5058717982664f93330e46ed366e95a3b008b506
ed1ct7 <ediktigranian@gmail.com>
2025-09-20 20:36:10 +0300

    Innitial commit

---
+++ 04,01 First/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04_01_First
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String dbFileName;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+
+        public MainWindow()
+        {
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+            dbFileName = "sample.sqlite";
+
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!System.IO.File.Exists(dbFileName))
+                SQLiteConnection.CreateFile(dbFileName);
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Vresion=3");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+            }
+            catch (SQLiteException ex)
+            { 
+            
+            }
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReadAllButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
