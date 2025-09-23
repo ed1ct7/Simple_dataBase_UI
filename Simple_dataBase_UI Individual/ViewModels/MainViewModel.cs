f2bb0c503945d269d4ac8c2349b5357c950944a5
ed1ct7 <ediktigranian@gmail.com>
2025-09-23 15:42:02 +0300

    simple database UI Individual project

---
+++ Simple_dataBase_UI Individual/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Simple_dataBase_UI_Individual.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Simple_dataBase_UI_Individual.ViewModels
+{
+    class MainViewModel
+    {
+
+        MainViewModel()
+        {
+            PrepareProgramm();
+            CheckConnectionCommand = new RelayCommand(ChechConnection);
+        }
+
+        public ICommand CheckConnectionCommand { get; }
+
+        private void ChechConnection(object parameter)
+        {
+
+        }
+
+        private void PrepareProgramm()
+        {
+
+        }
+    }
+}
